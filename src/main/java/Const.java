4373d4fb36732f358d177c82ead8830dbfed1a74
xiwangly2 <59991942+xiwangly2@users.noreply.github.com>
2023-12-25 09:49:39 +0800

    代码（混合更改

--- src/main/java/Const.java
+++ src/main/java/Const.java
@@ -6,9 +6,14 @@ public class Const {
     public  static String[] tableBorrowTitle = {"借阅编号","读者","图书","借出时间","归还时间"};
     //borrow id readerid bookid borrowTime backTime
     public  static String[] tableReaderTitle = {"读者编号","姓名","性别","证件类型","证件号","电话","邮箱"};
+    public  static String[] tablemanagerTitle = {"编号","姓名","密码","删除","修改"};
     //reader id   name sex paperType  paperNO tel email
     //菜单文本
-    public  static  String[]  menuTitle = {"系统设置","读者管理","图书管理","借阅管理","用户管理"};
+    public  static  String[]  menuTitle = {"系统信息","读者管理","图书管理","借阅管理","用户管理"};
     //子菜单文本
-    public  static  String[] menuItemsTitle ={"1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16"};
+    public  static  String[] menuItemsUser ={"注册用户","修改用户","查询用户","删除用户"};
+    public  static  String[] menuItemsBook ={"添加图书","修改图书","删除图书"};
+    public  static  String[] menuItemsBorrow ={"图书借阅","图书归还"};
+    public  static  String[] menuItemsReader = {"添加读者","修改读者","删除读者"};
+    public  static  String[] menuItemsSystem = {"信息设置","关于我们"};
 }
